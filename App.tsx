0bfa92e2de6a21330cb4c15a746750f272fa2a79
Lera Morozova <lera0603mo@gmail.com>
2020-06-27 16:07:35 +0300

    Drawer added

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import React,{ useState, useEffect, useContext } from 'react';
 import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
 import {GoogleSignin} from '@react-native-community/google-signin'
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 import firestore, { FirebaseFirestoreTypes, firebase } from '@react-native-firebase/firestore'
 import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth'
 import {ICoupon} from './Types/ICoupon'
@@ -11,7 +13,7 @@ import WelcomeScreen from './Views/WelcomeScreen'
 import CouponList from './Views/CouponList';
 
 GoogleSignin.configure({
-  
+  webClientId: '881842366351-qjhprb56e8re8fen7o0j7trenvi7s9ff.apps.googleusercontent.com'
   // Needs to be encapsulated into .env file
 })
 
@@ -30,10 +32,28 @@ interface State {
   user: FirebaseAuthTypes.User | null,
 }
 
-
+const Drawer = createDrawerNavigator();
 
 
 const App : React.FC = () => {
+  return (
+    <NavigationContainer independent={true}>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={MainView} />
+        <Drawer.Screen name="Redeem Code" component={RedeemCode}/>
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+const RedeemCode : React.FC = () => {
+  return (
+    <View></View>
+  )
+}
+
+const MainView : React.FC = () => {
 
 const {initializing, user} = useAuth();
 
